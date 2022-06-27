aad3f38fef6d1ed63041780da00e4b0140a2fa48
Burchin Ismail <burcinismail8@gmail.com>
2022-06-27 19:05:11 +0300

    Connected with API. Finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import PatternDividerDesktop from "./images/pattern-divider-desktop.svg";
 import DiceButton from "./images/icon-dice.svg";
+import { useState } from "react";
 function App() {
+  const [adviceNum, setAdviceNum] = useState(128);
+  const [adviceText, setAdviceText] = useState(
+    "It's unlucky to be superstitious."
+  );
+
+  const handleClick = async () => {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const json = await response.json();
+    setAdviceNum(json.slip.id);
+    setAdviceText(json.slip.advice);
+  };
   return (
     <div className="App font-sans bg-gray w-full h-screen flex items-center justify-center">
       <div className="card flex flex-col justify-center space-y-10 items-center bg-card-gray w-1/2 h-1/2 rounded-2xl px-5 pt-8 pb-10">
         <h4 className="advice-num text-card-num text-xl tracking-widest">
-          ADVICE #177
+          ADVICE #{adviceNum}
         </h4>
         <p className="advice-content text-card-content text-3xl text-center">
-          "It is easy to sit up and take notice, what's difficult is getting up
-          and taking action."
+          "{adviceText}"
         </p>
         <img src={PatternDividerDesktop} alt="divider"></img>
-        <div className="button-div p-6 bg-card-num rounded-full cursor-pointer hover:shadow-3xl">
+        <div
+          onClick={handleClick}
+          className="button-div p-6 bg-card-num rounded-full cursor-pointer hover:shadow-3xl"
+        >
           <img src={DiceButton} alt="dice button"></img>
         </div>
       </div>
